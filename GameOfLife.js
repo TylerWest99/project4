f18a605491a2ff79009d9627168af66a2b369fcf
Tyler West <tyler@TylerPC.localdomain>
2021-03-15 16:37:49 -0400

    does ints

--- GameOfLife.js
+++ GameOfLife.js
@@ -91,17 +91,17 @@ class GameOfLife {
                 //sets num of neighbors
                 numNeighbors = this.getNeighbors(i,j);
     
-                if(this.grid[i][j] == '1' && numNeighbors < 2){
-                    temp[i][j] = '0';
+                if(this.grid[i][j] == 1 && numNeighbors < 2){
+                    temp[i][j] = 0;
                 }
-                if(this.grid[i][j] == '1' && numNeighbors > 3){
-                    temp[i][j] = '0';
+                if(this.grid[i][j] == 1 && numNeighbors > 3){
+                    temp[i][j] = 0;
                 } 
-                if(this.grid[i][j] == '0' && numNeighbors == 3){
-                    temp[i][j] = '1';
+                if(this.grid[i][j] == 0 && numNeighbors == 3){
+                    temp[i][j] = 1;
                 }
-                if(this.grid[i][j] == '1' && (numNeighbors == 2 || numNeighbors == 3)){
-                    temp[i][j] = '1';
+                if(this.grid[i][j] == 1 && (numNeighbors == 2 || numNeighbors == 3)){
+                    temp[i][j] = 1;
                 }
             }
         }
